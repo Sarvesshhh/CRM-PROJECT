60637117b7254ceadef05fc8c33b48740fd2491c
Sarvesshhh <sarveshvaidhi06@gmail.com>
2026-03-26 20:22:53 +0530

    added cors file

--- crm-frontend/.next/static/chunks/webpack.js
+++ crm-frontend/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b21dd9662b320f22"; }
+/******/ 		__webpack_require__.h = function() { return "fc84de952a9c81eb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
